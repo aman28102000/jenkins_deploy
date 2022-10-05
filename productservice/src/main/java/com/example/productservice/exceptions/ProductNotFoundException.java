46ee9aac720505ae82aaeaefb3c556df6bae5972
Aman <amankrsonpur4@gmail.com>
2022-10-05 18:27:47 +0530

    changes

--- productservice/src/main/java/com/example/productservice/exceptions/ProductNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.productservice.exceptions;
-
-public class ProductNotFoundException extends RuntimeException{
-
-	public ProductNotFoundException(int id) {
-		super("Product with id " + id + " not found.");
-	}
-
-}
